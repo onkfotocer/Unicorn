d2040e04cc29b30e30dd6945d5aa7edb19907533
onkfotocer <onkfulan.fotocer@gmail.com>
2021-04-28 09:17:08 -0400

    Removed repo

--- FULL RESULTS/Xavier/module.py
+++
@@ -1,5 +0,0 @@
-from Params import params as pm
-import pandas as pd
-
-df = pd.DataFrame(pm)
-df.to_csv("xavier.csv")
--- FULL RESULTS/utils/change_core_status.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cpu_name=$1 
-operation=$2
-filename="/sys/devices/system/cpu/$cpu_name/online"
-echo $operation > $filename
-
